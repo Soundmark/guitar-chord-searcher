a5eb2b47bc09627ab7832a9ba459acf8887ed756
郑家豪 <beatboxzjh@163.com>
2022-10-21 23:26:07 +0800

    chore: initial commit

---
+++ src/pages/index/index.tsx
@@ -0,0 +1,14 @@
+import { View } from "@tarojs/components";
+
+import "taro-ui/dist/style/components/button.scss"; // 按需引入
+import "./index.less";
+
+export default function Index() {
+  return (
+    <View>
+      <View>
+        <View className="text-center">中</View>
+      </View>
+    </View>
+  );
+}
