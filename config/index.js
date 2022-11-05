aa68027df74faa5c4d599948d0f6fd511e2384b9
郑家豪 <beatboxzjh@163.com>
2022-11-05 22:51:46 +0800

    chore: 配置src的alias

--- config/index.js
+++ config/index.js
@@ -1,3 +1,6 @@
+/* eslint-disable import/no-commonjs */
+const path = require("path");
+
 const config = {
   projectName: "myApp",
   date: "2022-10-21",
@@ -5,42 +8,45 @@ const config = {
   deviceRatio: {
     640: 2.34 / 2,
     750: 1,
-    828: 1.81 / 2
+    828: 1.81 / 2,
   },
   sourceRoot: "src",
   outputRoot: "dist",
+  alias: {
+    "@": path.resolve(__dirname, "..", "src"),
+  },
   plugins: [],
   defineConstants: {},
   copy: {
     patterns: [],
-    options: {}
+    options: {},
   },
   framework: "react",
   compiler: "webpack5",
   cache: {
-    enable: false // Webpack 持久化缓存配置，建议开启。默认配置请参考：https://docs.taro.zone/docs/config-detail#cache
+    enable: false, // Webpack 持久化缓存配置，建议开启。默认配置请参考：https://docs.taro.zone/docs/config-detail#cache
   },
   plugins: ["taro-plugin-tailwind"],
   mini: {
     postcss: {
       pxtransform: {
         enable: true,
-        config: {}
+        config: {},
       },
       url: {
         enable: true,
         config: {
-          limit: 1024 // 设定转换尺寸上限
-        }
+          limit: 1024, // 设定转换尺寸上限
+        },
       },
       cssModules: {
         enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
         config: {
           namingPattern: "module", // 转换模式，取值为 global/module
-          generateScopedName: "[name]__[local]___[hash:base64:5]"
-        }
-      }
-    }
+          generateScopedName: "[name]__[local]___[hash:base64:5]",
+        },
+      },
+    },
   },
   h5: {
     publicPath: "/",
@@ -48,20 +54,20 @@ const config = {
     postcss: {
       autoprefixer: {
         enable: true,
-        config: {}
+        config: {},
       },
       cssModules: {
         enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
         config: {
           namingPattern: "module", // 转换模式，取值为 global/module
-          generateScopedName: "[name]__[local]___[hash:base64:5]"
-        }
-      }
-    }
-  }
+          generateScopedName: "[name]__[local]___[hash:base64:5]",
+        },
+      },
+    },
+  },
 };
 
-module.exports = function(merge) {
+module.exports = function (merge) {
   if (process.env.NODE_ENV === "development") {
     return merge({}, config, require("./dev"));
   }
